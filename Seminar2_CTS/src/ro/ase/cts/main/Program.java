c26c4f0d1f29ed010c882989229591e79e26e92a
Catalin0910 <catalincebuc99@gmail.com>
2021-03-04 08:22:42 +0200

    Single Responsability Principal

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -4,15 +4,15 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.classes.Angajat;
-import ro.ase.cts.utils.Utils;
+import ro.ase.cts.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			for (Angajat angajat : listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
---
+++ Seminar2_CTS/src/ro/ase/cts/readers/AplicantReader.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.readers;
+
+public abstract class AplicantReader {
+	private String fileName;
+
+	public AplicantReader(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+	
+	
+	
+}
