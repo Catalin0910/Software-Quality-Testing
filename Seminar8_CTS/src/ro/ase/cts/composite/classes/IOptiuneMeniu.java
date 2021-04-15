54d83711cde8f8a0e5cb5d87cc0f9d364ee684b4
Catalin0910 <catalincebuc99@gmail.com>
2021-04-15 08:07:57 +0300

    Composite dp

---
+++ Seminar8_CTS/src/ro/ase/cts/composite/classes/IOptiuneMeniu.java
@@ -0,0 +1,9 @@
+package ro.ase.cts.composite.classes;
+
+public interface IOptiuneMeniu {
+	void adugaNod(IOptiuneMeniu optiune) throws Exception;
+	void stergenod(IOptiuneMeniu optiune) throws Exception;
+	IOptiuneMeniu getNod(int index) throws Exception;
+	void descriere();
+
+}
