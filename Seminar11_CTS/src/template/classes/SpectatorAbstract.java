3a124065f90c9176cd354061118c8bb614733013
Catalin0910 <catalincebuc99@gmail.com>
2021-05-13 09:12:43 +0300

    Template method

---
+++ Seminar11_CTS/src/template/classes/SpectatorAbstract.java
@@ -0,0 +1,18 @@
+package template.classes;
+
+public abstract class SpectatorAbstract {
+	
+	public final void intrareSpectatoriPeStadion() {
+		aseszareLaCoada();
+		prezintaBilet();
+		realizeazaControlCorporal();
+		intraPeStadion();
+		ocupaLoc();
+	}
+	
+	public abstract void aseszareLaCoada();
+	public abstract void prezintaBilet();
+	public abstract void realizeazaControlCorporal();
+	public abstract void intraPeStadion();
+	public abstract void ocupaLoc();
+}
