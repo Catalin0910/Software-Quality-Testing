2c3bbe7c8434f08d11c8949475485b3c35b9f0dd
Catalin0910 <catalincebuc99@gmail.com>
2021-05-13 08:32:14 +0300

    Facade

---
+++ Seminar11_CTS/src/facade/main/Program.java
@@ -0,0 +1,17 @@
+package facade.main;
+
+import facade.classes.FacadeInternare;
+import facade.classes.Pacient;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Pacient p1 = new Pacient("Dani", false);
+		Pacient p2 = new Pacient("Catalin", true);
+		
+		System.out.println(FacadeInternare.verificaPacient(p1));
+		System.out.println(FacadeInternare.verificaPacient(p2));
+
+	}
+
+}
